897371d95fac8d20504989ebff779843afce5c2b
Lokesh Bysani <bysani3497@gmail.com>
2022-04-28 19:35:17 +0530

    updates function methods

--- functions.js
+++ functions.js
@@ -59,3 +59,30 @@ checkIn(flight,personJonas);
 console.log(flight);
 // As its an object . Changed value is refelected outside of the function as well. 
 console.log(personJonas);
+
+/*Higher Order Functions 
+1. Functions that accepts other functions as an input Ex: Event listeners
+2. Functions that returns other functions as an output
+3. Or both 1 and 2
+
+Helps with Abstraction. 
+*/
+
+const convertLower = function(str){
+    return str.toLowerCase();
+}
+
+const convertUpper = function(str){
+    return str.toUpperCase();
+}
+
+//This is a higher order function which transforms the string but really does not concern about how actually the string is transformed .
+//Actual details about the transformation is included in other functions
+const transformer = function(str,fn){
+    console.log('Before transformation :'+str);
+    console.log('After Transformation:'+fn(str));
+    console.log('Transformed By:'+fn.name)
+}
+
+transformer('Lokesh Bysani',convertLower)
+transformer('Lokesh Bysani',convertUpper)
