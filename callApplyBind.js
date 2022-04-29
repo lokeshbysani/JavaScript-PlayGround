b1eef23a67a325409f1d31c882ec875101f63e44
Lokesh Bysani <bysani3497@gmail.com>
2022-04-29 21:35:24 +0530

    FunctionMethods

---
+++ callApplyBind.js
@@ -0,0 +1,12 @@
+//call, Apply and Bind functionalities in functions
+
+const indigo = {
+    airline : 'indigo',
+    iatcode: 'IN456',
+    bookings : [],  // to store bookings for this flight
+
+    book(){
+        
+    }
+
+}
